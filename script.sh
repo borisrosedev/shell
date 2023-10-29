60005a0c2665b8c8e25ef74cd401efa803e038ab
boralex <boris.alexandre.rose@gmail.com>
2023-10-29 12:01:03 +0100

    feat/1

---
+++ script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# debugging
+#set -x 
+
+echo "Enter a number: "
+read num
+
+case $num in 
+	9)
+		echo "9"
+		;;
+	*)
+		echo -n "✅ -->"
+		echo "ce n'est pas 9"
+		
+		;;
+esac
+
+if [ $? -ne 0 ]; then
+	echo -e "🔴 Error occured"
+fi
